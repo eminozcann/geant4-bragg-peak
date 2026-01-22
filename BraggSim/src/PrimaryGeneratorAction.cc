ed4109eaf7dc248a077919d347ec261e9c18bc98
Emin Özcan <eminozcan314@gmail.com>
2026-01-22 17:17:36 +0300

    Deney duzenegi gama isini icin degistirildi (2 MeV)

--- BraggSim/src/PrimaryGeneratorAction.cc
+++ BraggSim/src/PrimaryGeneratorAction.cc
@@ -5,9 +5,9 @@
 PrimaryGeneratorAction::PrimaryGeneratorAction() {
     fParticleGun = new G4ParticleGun(1);
 
-    G4ParticleDefinition* particle = G4ParticleTable::GetParticleTable()->FindParticle("proton");
+    G4ParticleDefinition* particle = G4ParticleTable::GetParticleTable()->FindParticle("gamma");
     fParticleGun->SetParticleDefinition(particle);
-    fParticleGun->SetParticleEnergy(150. * MeV);
+    fParticleGun->SetParticleEnergy(2. * MeV);
     fParticleGun->SetParticleMomentumDirection(G4ThreeVector(0., 0., 1.));
     fParticleGun->SetParticlePosition(G4ThreeVector(0., 0., -16. * cm));
 }
